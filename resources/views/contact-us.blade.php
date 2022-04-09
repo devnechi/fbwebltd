abea9e84baa3073b3e0c9d21b49c22b97d588e18
future basics ltd <futurebasics@gmail.com>
2022-04-09 10:35:47 +0300

    plug

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -409,7 +409,7 @@
                                         </div> --}}
                                         <div class="form-group{{ $errors->has('g-recaptcha-response') ? ' has-error' : '' }}">
                                             <div class="col-md-6">
-                                                {!! RecaptchaV3::field('register') !!}
+                                                {!! RecaptchaV3::field('contactfbc') !!}
                                                 @if ($errors->has('g-recaptcha-response'))
                                                     <span class="help-block">
                                                         <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
